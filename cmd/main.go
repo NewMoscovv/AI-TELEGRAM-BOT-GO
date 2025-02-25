15a6d71a909adefb1deb02ee51d12c1343bf4057
Michael LapTop <kir.commersion@internet.ru>
2025-02-25 21:29:56 +0300

    added base bot arch

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"DeepSee_MAI/config"
 	"DeepSee_MAI/pkg/logger"
+	tele "gopkg.in/telebot.v3"
+	"time"
 )
 
 func main() {
@@ -14,6 +16,22 @@ func main() {
 		lgr.Err.Fatalf("Ошибка при инициализации конфигурации\n%v", err)
 	}
 
-	lgr.Info.Println(cfg)
+	// настройка характеристик бота
+	pref := tele.Settings{
+		Token:     cfg.TelegramToken,
+		Poller:    &tele.LongPoller{Timeout: 10 * time.Second},
+		ParseMode: tele.ModeHTML,
+	}
 
+	// создание экземлпяра бота
+	bot, err := tele.NewBot(pref)
+	if err != nil {
+		lgr.Err.Fatalf("Ошибка при запуске бота\n%v", err)
+	}
+
+	bot.Handle("/start", func(c tele.Context) error {
+		return c.Send("Base implementation")
+	})
+
+	bot.Start()
 }
