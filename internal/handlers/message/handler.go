c59ae2a2d069d0a9af86bc424bd74bf195770d51
Michael LapTop <kir.commersion@internet.ru>
2025-02-25 22:23:46 +0300

    added openrouter logic into handlers

--- internal/handlers/message/handler.go
+++ internal/handlers/message/handler.go
@@ -5,6 +5,7 @@ import (
 	"DeepSee_MAI/internal/openrouter"
 	"DeepSee_MAI/pkg/logger"
 	tele "gopkg.in/telebot.v3"
+	"strings"
 )
 
 type MsgHandler interface {
@@ -45,5 +46,20 @@ func (h *Handler) HandleStart(c tele.Context) error {
 func (h *Handler) HandleText(c tele.Context) error {
 	h.lgr.Info.Printf("%s | %s", c.Sender().Username, c.Text())
 
-	return c.Send("Привет?")
+	for {
+		// Печатает...
+		c.Notify(tele.Typing)
+
+		response, err := h.OpnRtr.GetResponse(c.Text())
+		if err != nil {
+			h.lgr.Err.Printf("%s", err.Error())
+			return c.Send("Ой, что-то пошло не так. Обратитесь в поддержку - <b>@new_moscovv</b>")
+		}
+		if response == "" {
+			h.lgr.Err.Printf("пустой ответ от ИИ, выполнение повторного запроса...")
+		} else {
+			h.lgr.Info.Printf("%s | \"%s\"", h.Bot.Me.Username, strings.Replace(response, "\n\n", "\n", -1))
+			return c.Send(response)
+		}
+	}
 }
