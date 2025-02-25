ab8e95d30ecf04c5c93bfb3b39907fb87f0fd3d6
Michael LapTop <kir.commersion@internet.ru>
2025-02-25 21:11:15 +0300

    added config.go

---
+++ config/config.go
@@ -0,0 +1,29 @@
+package config
+
+import (
+	"github.com/joho/godotenv"
+	"os"
+)
+
+// Config хранит данные конфигурации
+type Config struct {
+	TelegramToken string
+}
+
+func NewConfig() *Config {
+
+	err := godotenv.Load(".env")
+	if err != nil {
+		// TODO: log info via logger...
+	}
+
+	telegramToken := os.Getenv("TELEGRAM_TOKEN")
+	if telegramToken == "" {
+		// TODO: log info via logger...
+	}
+
+	return &Config{
+		TelegramToken: telegramToken,
+	}
+
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,5 @@
 module DeepSee_MAI
+
+go 1.23.4
+
+require github.com/joho/godotenv v1.5.1 // indirect
