1b3347c634958879e0a634292c487b7af66140e7
Michael LapTop <kir.commersion@internet.ru>
2025-02-25 21:20:23 +0300

    func NewConfig() (*config, error) instead of func NewConfig() *Config{}

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"errors"
 	"github.com/joho/godotenv"
 	"os"
 )
@@ -9,20 +10,20 @@ type Config struct {
 	TelegramToken string
 }
 
-func NewConfig() *Config {
+func NewConfig() (*Config, error) {
 
 	err := godotenv.Load(".env")
 	if err != nil {
-		// TODO: log info via logger...
+		return nil, err
 	}
 
 	telegramToken := os.Getenv("TELEGRAM_TOKEN")
 	if telegramToken == "" {
-		// TODO: log info via logger...
+		return nil, errors.New("отсутствует TELEGRAM_TOKEN")
 	}
 
 	return &Config{
 		TelegramToken: telegramToken,
-	}
+	}, nil
 
 }
