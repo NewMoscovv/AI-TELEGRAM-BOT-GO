9fa061f58ed3aee5f66be0b069157c57ddaa677b
Michael LapTop <kir.commersion@internet.ru>
2025-02-25 21:36:43 +0300

    add bot.Init() into another file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"DeepSee_MAI/config"
+	b "DeepSee_MAI/internal/bot"
+	"DeepSee_MAI/internal/config"
 	"DeepSee_MAI/pkg/logger"
 	tele "gopkg.in/telebot.v3"
-	"time"
 )
 
 func main() {
@@ -13,20 +13,13 @@ func main() {
 
 	cfg, err := config.InitConfig()
 	if err != nil {
-		lgr.Err.Fatalf("Ошибка при инициализации конфигурации\n%v", err)
+		lgr.Err.Fatalf("Ошибка инициализации конфигурации\n%v", err)
 	}
 
-	// настройка характеристик бота
-	pref := tele.Settings{
-		Token:     cfg.TelegramToken,
-		Poller:    &tele.LongPoller{Timeout: 10 * time.Second},
-		ParseMode: tele.ModeHTML,
-	}
-
-	// создание экземлпяра бота
-	bot, err := tele.NewBot(pref)
+	// инициализация бота
+	bot, err := b.InitBot(cfg)
 	if err != nil {
-		lgr.Err.Fatalf("Ошибка при запуске бота\n%v", err)
+		lgr.Err.Fatalf("Ошибка запуска бота\n%v", err)
 	}
 
 	bot.Handle("/start", func(c tele.Context) error {
---
+++ internal/bot/Init.go
@@ -0,0 +1,25 @@
+package bot
+
+import (
+	"DeepSee_MAI/internal/config"
+	tele "gopkg.in/telebot.v3"
+	"time"
+)
+
+func InitBot(cfg *config.Config) (*tele.Bot, error) {
+
+	// настройка характеристик бота
+	pref := tele.Settings{
+		Token:     cfg.TelegramToken,
+		Poller:    &tele.LongPoller{Timeout: 10 * time.Second},
+		ParseMode: tele.ModeHTML,
+	}
+
+	// создание экземлпяра бота
+	bot, err := tele.NewBot(pref)
+	if err != nil {
+		return nil, err
+	}
+
+	return bot, nil
+}
