1348812e16242ca86ecdacd360075a9eb3fca3cb
Michael LapTop <kir.commersion@internet.ru>
2025-02-25 21:57:02 +0300

    added extra fields into config

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,6 +8,9 @@ import (
 
 type Config struct {
 	TelegramToken string
+	OpnRtrToken   string
+	APIUrl        string
+	Model         string
 }
 
 func InitConfig() (*Config, error) {
@@ -22,8 +25,26 @@ func InitConfig() (*Config, error) {
 		return nil, errors.New("отсутствует TELEGRAM_TOKEN")
 	}
 
+	openRouterToken := os.Getenv("OPENROUTER_TOKEN")
+	if openRouterToken == "" {
+		return nil, errors.New("отсутствует токен OpenRouter")
+	}
+
+	apiUrl := os.Getenv("API_URL")
+	if apiUrl == "" {
+		return nil, errors.New("отсутствует API_URL")
+	}
+
+	model := os.Getenv("MODEL")
+	if model == "" {
+		return nil, errors.New("отсутствует название Модели")
+	}
+
 	return &Config{
 		TelegramToken: telegramToken,
+		OpnRtrToken:   openRouterToken,
+		APIUrl:        apiUrl,
+		Model:         model,
 	}, nil
 
 }
